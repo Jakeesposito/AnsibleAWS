11916294ab82f815b7405e2c2aabd0a3477ebdf8
Jakeesposito <41878068+Jakeesposito@users.noreply.github.com>
2018-07-29 18:08:16 -0500

    Added finalization step 6

--- AnsibleBash.sh
+++ AnsibleBash.sh
@@ -12,25 +12,42 @@
 # 5. Install Ansible on Ubuntu Server
 
 
+# 1. Update the EC2 Ubuntu Server Image 
 echo Updating Ubuntu Distribution...
 sudo apt-get update
 echo Updating Ubuntu Distribution...[COMPLETE]
 sleep 1
+# 2. Install 'software-properties-common'
 echo Installing Required Dependencies...
 sudo apt-get install software-properties-common
 echo Installing Required Dependencies...[COMPLETE]
 sleep 1
+# 3. Add Ansible Repository
 echo Adding Ansible Repository...
 sudo apt-add-repository ppa:ansible/ansible
 echo Adding Ansible Repository...[COMPLETE]
 sleep 1
+# 4. Update Ansible Repository
 echo Updating Ansible Repository...
 sudo apt-get update
 echo Updating Ansible Repository...[COMPLETE]
 sleep 1
+# 5. Install Ansible on Ubuntu Server
 echo Installing Ansible...
 sudo apt-get install ansible
 echo Installing Ansible...[COMPLETE]
 sleep 1
 echo ---Ansible has been successfully installed on the machine---
+sleep 1
+# 6. Final Update
+echo Finalizing...
 sleep .5
+echo .
+sleep .5
+echo .
+sleep .5
+echo .
+sudo apt-get update
+echo DONE
+sleep .5
+echo |||---SCRIPT COMPLETED---|||

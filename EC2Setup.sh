a2a38e81e0e00692e98079014bebf93fabc9f99e
Jakeesposito <41878068+Jakeesposito@users.noreply.github.com>
2018-08-09 10:11:08 -0700

    added apt-get update

--- EC2Setup.sh
+++ EC2Setup.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
+sudo apt-get update
 echo Installing Amazon Web Services CLI...
 sudo apt-get install awscli > /dev/null
+sleep 3
 echo Installing Amazon Web Services CLI...[COMPLETE]
-sleep 1
+sleep 3
 echo Installing jq...
 sudo apt-get install jq > /dev/null
 echo Installing jq...[COMPLETE]
-sleep 1
+sleep 3
 echo Enter Access Keys Below...
 aws configure
 echo Testing AWS Connection...

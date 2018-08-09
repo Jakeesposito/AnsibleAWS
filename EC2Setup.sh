628ee1c5db9e98fddd3b036621a514355341f8b3
Jakeesposito <41878068+Jakeesposito@users.noreply.github.com>
2018-08-09 10:17:05 -0700

    edited /dev/null

--- EC2Setup.sh
+++ EC2Setup.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-sudo apt-get update
+sudo apt-get update > /dev/null
 echo Installing Amazon Web Services CLI...
-sudo apt-get install awscli > /dev/null
+sudo apt-get -y install awscli > /dev/null
 sleep 3
 echo Installing Amazon Web Services CLI...[COMPLETE]
 sleep 3
 echo Installing jq...
-sudo apt-get install jq > /dev/null
+sudo apt-get -y install jq > /dev/null
 echo Installing jq...[COMPLETE]
 sleep 3
 echo Enter Access Keys Below...

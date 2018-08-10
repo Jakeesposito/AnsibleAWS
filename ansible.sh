8bc0ae04a34f97bcb24fcf4cd587588163de3bf1
Jakeesposito <41878068+Jakeesposito@users.noreply.github.com>
2018-08-10 18:03:32 -0400

    colorized output

--- ansible.sh
+++ ansible.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
+
+#Text Color Variables
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
+#Update Ubuntu & Install Ansible PPA
 echo Updating Ubuntu Distribution...
 sudo apt-get update > /dev/null 2>&1
-echo Updating Ubuntu Distribution...[COMPLETE]
+echo Updating Ubuntu Distribution...${grn}[COMPLETE]${end}
 sleep 3
 echo Installing Required Dependencies...
 sudo apt-get install software-properties-common > /dev/null 2>&1
-echo Installing Required Dependencies...[COMPLETE]
+echo Installing Required Dependencies...${grn}[COMPLETE]${end}
 sleep 3
 echo Adding Ansible Repository...
 echo | sudo apt-add-repository ppa:ansible/ansible > /dev/null 2>&1
 echo | sudo apt-get-update > /dev/null 2>&1
-echo Adding Ansible Repository...[COMPLETE]
+echo Adding Ansible Repository...${grn}[COMPLETE]${end}
 sleep 3
 echo Updating Ansible Repository...
 sudo apt-get update > /dev/null 2>&1
-echo Updating Ansible Repository...[COMPLETE]
+echo Updating Ansible Repository...${grn}[COMPLETE]${end}
 sleep 3
 echo Installing Ansible...
 sudo apt-get install ansible > /dev/null 2>&1
-echo Installing Ansible...[COMPLETE]
+echo Installing Ansible...${grn}[COMPLETE]${end}
 sleep 3
 echo Finalizing...
 sleep .5
@@ -29,4 +39,4 @@ sleep .5
 echo .
 sudo apt-get update > /dev/null 2>&1
 sleep 3
-echo ANSIBLE INSTALLATION AND CONFIGURATION COMPLETE
+echo ${mag}ANSIBLE INSTALLATION AND CONFIGURATION COMPLETE${end}

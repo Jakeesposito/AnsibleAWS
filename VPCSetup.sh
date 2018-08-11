dbe43f95a306a2231c4f33deccf1e5b138fab46e
Jakeesposito <41878068+Jakeesposito@users.noreply.github.com>
2018-08-11 15:49:28 -0500

    colorized variables

--- VPCSetup.sh
+++ VPCSetup.sh
@@ -1,10 +1,22 @@
 #! /bin/bash
+
+# Color Variables
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
+# Create VPC and Extract VPCID
 echo Creating Virtual Private Cloud 10.0.0.0/16...
 aws ec2 create-vpc --cidr-block 10.0.0.0/16 > /dev/null
 sleep 3
-echo Creating Virtual Private Cloud 10.0.0.0/16...[COMPLETE]
+echo Creating Virtual Private Cloud 10.0.0.0/16...${grn}[COMPLETE]${end}
 sleep 2
 echo Extracting VPC ID...
 VPCID=$(aws ec2 describe-vpcs --query 'Vpcs[1].{VPCID:VpcId}' --output text) > /dev/null
 sleep 3
-echo VPC 10.0.0.0/16 created with VPC ID $VPCID
+echo VPC ${mag}10.0.0.0/16${end} created with VPC ID ${mag}$VPCID${end}
+
+# Create Subnets in VPC

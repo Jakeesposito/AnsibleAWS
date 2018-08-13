8dae6f735a45407692fbe3c850a716ef6d10d56f
Jakeesposito <41878068+Jakeesposito@users.noreply.github.com>
2018-08-13 12:54:08 -0500

    colorized for better UX

--- VPCSetup.sh
+++ VPCSetup.sh
@@ -9,36 +9,36 @@ cyn=$'\e[1;36m'
 end=$'\e[0m'
 
 # Create VPC and Extract VPCID
-echo Creating Virtual Private Cloud 10.0.0.0/16...
+echo ${blu}Creating Virtual Private Cloud 10.0.0.0/16...${end}
 aws ec2 create-vpc --cidr-block 10.0.0.0/16 > /dev/null
 sleep 3
-echo Creating Virtual Private Cloud 10.0.0.0/16...${grn}[COMPLETE]${end}
+echo ${blu}Creating Virtual Private Cloud 10.0.0.0/16...${end}${grn}[COMPLETE]${end}
 sleep 2
-echo Extracting VPC ID...
+echo ${blu}Extracting VPC ID...${end}
 VPCID=$(aws ec2 describe-vpcs --query 'Vpcs[1].{VPCID:VpcId}' --output text) > /dev/null
 sleep 3
-echo VPC ${mag}10.0.0.0/16${end} created with VPC ID ${mag}$VPCID${end}
+echo ${blu}VPC ${mag}10.0.0.0/16${end} ${blu}created with VPC ID ${mag}$VPCID${end}
 
 # Create Subnets in VPC
-echo Creating VPC Client Subnet...
+echo ${blu}Creating VPC Client Subnet...${end}
 sleep 3
 aws ec2 create-subnet --vpc-id ${VPCID} --cidr-block 10.0.1.0/24 | jq
 sleep 3
-echo Creating VPC Client Subnet...${grn}[COMPLETE]${end}
+echo ${blu}Creating VPC Client Subnet...${grn}[COMPLETE]${end}
 sleep 3
-echo Creating VPC Server Subnet...
+echo ${blu}Creating VPC Server Subnet...${end}
 sleep 3
 aws ec2 create-subnet --vpc-id ${VPCID} --cidr-block 10.0.2.0/24 | jq
 sleep 3
-echo Creating VPC Server Subnet...${grn}[COMPLETE]${end}
+echo ${blu}Creating VPC Server Subnet...${grn}[COMPLETE]${end}
 sleep 3
-echo Creating VPC Management Subnet...
+echo ${blu}Creating VPC Management Subnet...${end}
 sleep 3
 aws ec2 create-subnet --vpc-id ${VPCID} --cidr-block 10.0.3.0/24 | jq
 sleep 3
-echo Creating VPC Management Subnet...${grn}[COMPLETE]${end}
+echo ${blu}Creating VPC Management Subnet...${grn}[COMPLETE]${end}
 sleep 3
-echo .
+echo ${mag}.${end}
 echo ${mag}Client, Server, and Management Networks Created${end}
-echo .
+echo ${mag}.${end}
 sleep 1

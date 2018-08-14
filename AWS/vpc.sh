d3035e0548dddc4479618b5e30018c2ecc4aa278
Jakeesposito <41878068+Jakeesposito@users.noreply.github.com>
2018-08-13 20:04:33 -0500

    Update vpc.sh

--- AWS/vpc.sh
+++ AWS/vpc.sh
@@ -20,6 +20,20 @@ VPCID=$(aws ec2 describe-vpcs --query 'Vpcs[1].{VPCID:VpcId}' --output text) > /
 sleep 3
 echo ${mag}VPC ${red}10.0.0.0/16${end} ${mag}created with VPC ID ${red}$VPCID${end}
 
+# Create & Attach Internet Gateway
+sleep 3
+echo ${mag}Creating Internet Gateway...${end}
+aws ec2 create-internet-gateway | jq
+sleep 3
+IGWID=$(aws ec2 describe-internet-gateways --query 'InternetGateways[1].{IGWID:InternetGatewayId}' --output text) > /dev/null
+echo ${mag}Creating Internet Gateway...${grn}[COMPLETE]${end}
+sleep 3
+echo ${mag}Attaching Internet Gateway ${red}$IGWID${mag} to VPC ${red}$VPCID{mag}...
+aws ec2 attach-internet-gateway --internet-gateway-id ${IGWID} --vpc-id ${VPCID} --region us-east-2 > /dev/null
+sleep 3
+echo ${mag}Attaching Internet Gateway ${red}$IGWID${mag} to VPC ${red}$VPCID{mag}...${grn}[COMPLETE]${mag}
+sleep 3
+
 # Create Subnets in VPC
 echo ${mag}Creating VPC Client Subnet...${end}
 sleep 3
@@ -39,6 +53,22 @@ aws ec2 create-subnet --vpc-id ${VPCID} --cidr-block 10.0.3.0/24 | jq
 sleep 3
 echo ${mag}Creating VPC Management Subnet...${grn}[COMPLETE]${end}
 sleep 3
+
+# Create Route Tables & Routes
+echo ${mag}Creating a Custom Route Table...${end}
+aws ec2 create-route-table --vpc-id ${VPCID} | jq
+sleep 5
+RTID=$(aws ec2 describe-route-tables --query 'RouteTables[2].{RTID:RouteTableId}' --output text)
+echo ${mag}Creating a Custom Route Table...${grn}[COMPLETE]${end}
+sleep 1
+echo ${mag}Route Table ID = ${red}$RTID${end}
+sleep 3
+
+
+
+
+
+# Script Completion
 echo ${mag}...........................................................${end}
 echo ${mag}...........................................................${end}
 echo ${mag}.....${grn}Client, Server, and Management Networks Created${mag}.......${end}

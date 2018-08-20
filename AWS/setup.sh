2160978fef4be7cde4c4fc5fb7993c0985d3d63a
Jakeesposito <41878068+Jakeesposito@users.noreply.github.com>
2018-08-20 13:30:36 -0500

    Update setup.sh

--- AWS/setup.sh
+++ AWS/setup.sh
@@ -20,6 +20,18 @@ sudo apt-get -y install jq > /dev/null
 echo ${grn}[COMPLETE]${end}
 sleep 3
 
+# Install Azure CLI
+echo ${mag}Installing Azure CLI 2.0...${end}
+AZ_REPO=$(lsb_release -cs)
+echo ${mag}Version ${grn}${AZ_REPO}${end}
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list > /dev/null
+curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add - > /dev/null
+sudo apt-get install apt-transport-https > /dev/null
+sudo apt-get update && sudo apt-get install azure-cli
+sleep 3
+echo ${grn}[COMPLETE]${end}
+sleep 3
+
 # Authenticate into AWS
 echo ${mag}Enter Access Keys Below...${red}
 aws configure
@@ -27,6 +39,10 @@ echo ${mag}Testing AWS Connection...${end}
 sleep 3
 aws sts get-caller-identity | jq
 sleep 3
+
+# Authenticate into Azure
+az login | jq
+
 echo ${mag}...........................................................${end}
 echo ${mag}...........................................................${end}
 echo ${mag}............${grn}Amazon Web Services Setup Complete${mag}.............${end}

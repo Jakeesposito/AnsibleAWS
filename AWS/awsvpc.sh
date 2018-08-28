ce97249d02d7da87eba843ab62037340f31d0fb9
Jakeesposito <41878068+Jakeesposito@users.noreply.github.com>
2018-08-28 14:37:28 -0500

    Update awsvpc.sh

--- AWS/awsvpc.sh
+++ AWS/awsvpc.sh
@@ -1,19 +1,16 @@
 #! /bin/bash
 
 # Environment Variables
-# - colors
 red=$'\e[1;31m'
 grn=$'\e[1;32m'
 blu=$'\e[1;34m'
 mag=$'\e[1;35m'
 cyn=$'\e[1;36m'
 end=$'\e[0m'
-# - Complete
 complete=$(echo ${grn}[COMPLETE]${mag})
 
 # Create VPC
-echo ${mag}
-echo Creating VPC...
+echo ${mag}Creating VPC...
 vpc_info=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --output json)
 vpc_id=$(echo $vpc_info | jq '.Vpc.VpcId' | tr -d '"')
 aws ec2 create-tags --resources "$vpc_id" --tags Key=Name,Value=Demo_VPC
@@ -82,19 +79,10 @@ aws ec2 authorize-security-group-ingress --group-id ${sg_id} --protocol tcp --po
 echo ${complete}
 printf "\n"
 
-# Deploying VM for Ansible Control Machine
-echo Deploying VM for Ansible Control Machine...
-ans_instance_id=$(aws ec2 run-instances --image-id ami-5e8bb23b --count 1 --instance-type t2.micro --security-group-ids ${sg_id} --key-name AWSPrivateKey --subnet-id ${m_subnet_id} --associate-public-ip-address | jq '.Instances' | jq .[] | jq '.InstanceId' | tr -d '"')
-aws ec2 create-tags --resources "$ans_instance_id" --tags Key=Name,Value=Ansible_ControlMachine
-echo ${cyn}EC2 Instance ${grn}Ansible_ControlMachine${cyn} Created
-sleep 3
+# Complete
+echo ${grn}[VPC CONFIGURATION COMPLETED]
+sleep 2
 printf "\n"
-echo ${mag}Waiting for Public DNS...
-sleep 3
-ans_dns=$(aws ec2 describe-instances --instance-ids ${ans_instance_id} | jq .[] | jq .[] | jq '.Instances' | jq .[] | jq '.PublicDnsName' | tr -d '"')
-printf "\n"
-echo Ansible Control Machine can be Accessed via ssh at 
-printf "\n"
-echo ${mag}${ans_dns}${cyn}
+echo ${grn}[SCRIPT COMPLETE]${end}
 
 
